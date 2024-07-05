4c5ea462f8aa791074f89fdd9b8fd6fbf25c85b2
Rajesh <sserajesh19@gmail.com>
2024-07-05 17:37:06 +0530

    Setup .env file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 //server setup using express
 
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = 3000;
+
 const app = express();
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     console.log('Working..');
